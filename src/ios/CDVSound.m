12d48c044c52d23af10b5d491c07d13f2acc0287
Rob Gagne <rob@robgagne.com>
2019-10-11 08:30:44 +0900

    more tweaks to get music to keep playing

--- src/ios/CDVSound.m
+++ src/ios/CDVSound.m
@@ -374,9 +374,12 @@ BOOL keepAvAudioSessionAlwaysActive = NO;
                 /* rgagne: switching to use categoryAmbient and switching category*/                   
                 // NSString* sessionCategory = bPlayAudioWhenScreenIsLocked ? AVAudioSessionCategoryPlayback : AVAudioSessionCategorySoloAmbient;
                 // [self.avSession setCategory:sessionCategory error:&err];
-                [self.avSession setCategory:AVAudioSessionCategoryPlayAndRecord 
-                    withOptions:AVAudioSessionCategoryOptionDuckOthers | AVAudioSessionCategoryOptionDefaultToSpeaker | 
-                    AVAudioSessionCategoryOptionInterruptSpokenAudioAndMixWithOthers error:nil];
+                
+                //[self.avSession setCategory:AVAudioSessionCategoryPlayAndRecord 
+                //    withOptions:AVAudioSessionCategoryOptionDuckOthers | AVAudioSessionCategoryOptionDefaultToSpeaker | 
+                //    AVAudioSessionCategoryOptionInterruptSpokenAudioAndMixWithOthers error:nil];
+
+                [self.avSession setCategory:sessionCategory withOptions:AVAudioSessionCategoryOptionMixWithOthers error:&err];
 
                 if (![self.avSession setActive:YES error:&err]) {
                     // other audio with higher priority that does not allow mixing could cause this to fail
